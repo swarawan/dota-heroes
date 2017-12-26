7780a19c22eb1f83cef0d9bd12325a0c4ec3334a
Rio Swarawan Putra <swarawan.rio@gmail.com>
2017-12-26 14:46:35 +0700

    add menu action and swipe refresh

--- app/src/main/java/com/swarawan/dotaheroes/presentation/heroes/HeroesActivity.kt
+++ app/src/main/java/com/swarawan/dotaheroes/presentation/heroes/HeroesActivity.kt
@@ -1,7 +1,12 @@
 package com.swarawan.dotaheroes.presentation.heroes
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.widget.SearchView
 import com.swarawan.dotaheroes.R
 import com.swarawan.dotaheroes.base.InjectedActivity
 import com.swarawan.dotaheroes.di.component.ActivityComponent
@@ -23,24 +28,45 @@ class HeroesActivity : InjectedActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        presenter.attachView(heroView)
+
         recycler_hero.adapter = adapter
         recycler_hero.layoutManager = LinearLayoutManager(this)
         recycler_hero.setHasFixedSize(true)
 
-        presenter.attachView(heroView)
-        presenter.getHeroes()
+        swipe.setOnRefreshListener { onRefreshListener }
+        swipe.post { onRefreshListener.onRefresh() }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.menu_search)?.actionView as SearchView
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 
     override fun inject(activityComponent: ActivityComponent) {
         activityComponent.inject(this)
     }
 
-    private val heroView = object : HeroView {
-        override fun showLoading() {
+    private val onRefreshListener = SwipeRefreshLayout.OnRefreshListener { presenter.getHeroes() }
 
+    private val heroView = object : HeroView {
+
+        override fun showLoading() {
+            if (!swipe.isRefreshing) {
+                swipe.isRefreshing = true
+            }
         }
 
         override fun hideLoading() {
+            if (swipe.isRefreshing) {
+                swipe.isRefreshing = false
+            }
         }
 
         override fun displayHero(fetchedHeroes: List<Hero>) {
